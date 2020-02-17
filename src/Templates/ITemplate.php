0fcf6bb9e262f594c6ec72ce56a63a42077362ee
MartkCz <martk494@gmail.com>
2020-02-17 22:10:34 +0100

    ITemplate removed __constructor

--- src/Templates/ITemplate.php
+++ src/Templates/ITemplate.php
@@ -6,14 +6,10 @@ use Contributte\Invoice\Calculators\ICalculator;
 use Contributte\Invoice\Data\Company;
 use Contributte\Invoice\Data\Customer;
 use Contributte\Invoice\Data\Order;
-use Contributte\Invoice\Formatter;
-use Contributte\Invoice\ITranslator;
 use Contributte\Invoice\Renderers\IRenderer;
 
 interface ITemplate
 {
-
-	public function __construct(ITranslator $translator, Formatter $formatter);
 
 	public function build(ICalculator $calculator, IRenderer $renderer, Customer $customer, Order $order, Company $company): string;
 
